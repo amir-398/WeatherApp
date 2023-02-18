9338a4221962c5798a0ceb6805cd65e13986543a
amir-398 <amir.398@hotmail.fr>
2023-02-19 00:46:46 +0100

    finished

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-const height = document.querySelector("#height");
-const weight = document.querySelector("#weight");
-console.log(height, weight);
-weight.addEventListener("input", (e) => {
-  console.log(e.target.value);
-});
-height.addEventListener("input", (e) => {
-  console.log(e.target.value);
-});
+const BMIData = [
+  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+  { name: "Bonne santé", color: "green", range: [18.5, 25] },
+  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+  { name: "Obésité modérée", color: "orange", range: [30, 35] },
+  { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+  { name: "Obésité morbide", color: "purple", range: 40 },
+];
+const imc = document.querySelector("#imc");
+const interpretation = document.querySelector("#interpretation");
+const from = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+from.addEventListener("submit", handleForm);
+function handleForm(e) {
+  e.preventDefault();
+  calculateBMI();
+}
+function calculateBMI() {
+  const height = inputs[0].value;
+
+  const weight = inputs[1].value;
+  console.log(height, weight);
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    handleError();
+    return;
+  }
+  const bmi = weight / Math.pow(height / 100, 2).toFixed(1);
+  console.log("lol", bmi);
+  showResult(bmi);
+}
+
+function handleError() {
+  imc.textContent = "Oops";
+  imc.style.color = "inherit";
+  interpretation.textContent = "Remplissez correctement les champs de texte";
+}
+
+function showResult(BMI) {
+  const rank = BMIData.find((data) => {
+    if (BMI >= data.range[0] && BMI <= data.range[1]) return data;
+    else if (typeof data.range === "number" && BMI >= data.range) return data;
+  });
+  imc.textContent = BMI;
+  imc.style.color = `${rank.color}`;
+  interpretation.textContent = `Resultat : ${rank.name}`;
+}
